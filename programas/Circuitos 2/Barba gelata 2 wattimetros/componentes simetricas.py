bf4a5124203aa596da83a526539997970edb75dc
LACE-UFU <laceufu@gmail.com>
2019-11-18 12:54:40 -0300

    lis

--- programas/Circuitos 2/Barba gelata 2 wattimetros/componentes simetricas.py
+++ programas/Circuitos 2/Barba gelata 2 wattimetros/componentes simetricas.py
@@ -3,9 +3,14 @@ import numpy as np
 from libs import *
 
 a = s2c('1|120')
-T = [[1] * 3, [1, a ** 2, a], [1, a, a ** 2]]
-T = np.array(T)
+T = Matrix([[1] * 3, [1, a ** 2, a], [1, a, a ** 2]])  # matrix de transformacao
 
 
-T = Matrix([[1,2,3]],2,21)
-print(T)
+def real2Sime(input: (Matrix, list)):
+    input = Matrix(input).transpose()
+    global T
+    return T.inverse() * input
+
+
+r = real2Sime(['100', '100|-120', '100|120'])
+print(r)
--- programas/Circuitos 2/Barba gelata 2 wattimetros/libs.py
+++ programas/Circuitos 2/Barba gelata 2 wattimetros/libs.py
@@ -5,8 +5,12 @@ import numpy as np
 
 
 def s2c(c: str):
-    if isinstance(c, (float, int)):
+    if isinstance(c, (float, int, complex)):
         return c
+    if not isinstance(c, str):
+        raise ValueError(f'not suport to {type(c)}')
+    if not '|' in c:
+        return complex(c.replace(' ', '').replace('i', 'j'))
     c = c.split('|', 1)
     if len(c) == 0:
         c = [0, 0]
@@ -22,33 +26,75 @@ def pc(c: complex, precisao=4):
         return f'%.{precisao}f' % c
     elif isinstance(c, str):
         return c
-    return f'%.{precisao}g|%.{precisao}g°' % (abs(c), cmath.phase(c) * 180 / pi)
+    return f'%.{precisao}g|%.{precisao}f°' % (abs(c), cmath.phase(c) * 180 / pi)
 
 
-class Matrix:
-    def __init__(self, data: list = None, size_i: int = None, size_j: int = None):
+class Matrix(object):
+    def __init__(self, data: (list, int) = None, size_i: int = None, size_j: int = None):
+        if data is None and size_i is None and size_j is None:
+            raise AttributeError('Falta coisa ai man')
+
+        if isinstance(data, Matrix):
+            self.data = data.data.copy()
+        if isinstance(data, np.ndarray):
+            data = data.tolist()
+
         if isinstance(data, (int, float)):
             size_j = size_i
             size_i = data
             data = None
-        if data == size_j == size_i is None:
-            raise AttributeError('Falta coisa ai man')
+
+        if isinstance(data, list):
+            try:
+                len(data[0])
+            except TypeError:
+                data = [data]
         if size_i is None:
             size_i = len(data)
         if size_j == None:
-            size_j = len(data[0])
+            try:
+                size_j = len(data[0])
+            except TypeError:
+                size_j = 1
         if data is None:
             self.data = np.array([[0] * size_j] * size_i)
         if data:
             m = []
+
+            leni = len(data)
+            lenj = len(data)
             for i in range(size_i):
                 m.append([])
                 for j in range(size_j):
-                    if i < len(data) and j < len(data[i]):
-                        m[i].append(data[i][j])
+                    if i < leni and j < lenj:
+                        m[i].append(s2c(data[i][j]))
                     else:
                         m[i].append(0)
             self.data = np.array(m)
 
     def __repr__(self):
-        return np.array2string(self.data)
+        lt = []
+        m = 0
+        for i in self.data.tolist():
+            lt.append([])
+            for j in i:
+                lt[-1].append(pc(j,precisao=4))
+                m = max(len(lt[-1][-1]), m)
+        s = ''
+        for i in lt:
+            for j in i:
+                t = (m - len(j)) // 2
+                s += ' ' * t + j + '    ' + ' ' * t
+            s+='\n'
+        return s
+
+    def __mul__(self, other):
+        # print(isinstance(self.data,np.ndarray))
+        a = self.data * other.data
+        return Matrix(a)
+
+    def transpose(self):
+        return Matrix(self.data.transpose())
+
+    def inverse(self):
+        return Matrix(np.linalg.inv(self.data))
